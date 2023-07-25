db7495eb12093902b5596b59e9acd4bf171e5843
Emmanuel Dominic <82080301+Tocci997@users.noreply.github.com>
2023-07-25 11:52:32 +0100

    Update my_string.c

--- my_string.c
+++ my_string.c
@@ -1,15 +1,15 @@
 #include "main.h"
 int my_string(char *str)
 {
-	int index = 0, len = 0;
+	int index = 0, r_value = 0;
 	if(str)
 	{
 		while(str[index] != '\0')
 		{
 			my_putchar(str[index]);
-			len += 1;
+			r_value += 1;
 				index++;
 		}
 	}
-	return (len);
+	return (r_value);
 }

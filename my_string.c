3cb5e1f095ad3b36882c3ba44ec7af919ed1ad8e
Emmanuel Dominic <82080301+Tocci997@users.noreply.github.com>
2023-07-25 11:46:58 +0100

    Changed r_value to len my_string.c

--- my_string.c
+++ my_string.c
@@ -1,15 +1,15 @@
 #include "main.h"
 int my_string(char *str)
 {
-	int index = 0, r_value = 0;
+	int index = 0, len = 0;
 	if(str)
 	{
 		while(str[index] != '\0')
 		{
 			my_putchar(str[index]);
-			r_value += 1;
+			len += 1;
 				index++;
 		}
 	}
-	return (r_value);
+	return (len);
 }

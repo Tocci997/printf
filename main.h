ed6852271ce54b3a69937e069335239ed01fd675
chioma <chukwudichiomac@gmail.com>
2023-07-25 06:31:20 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+int my_putchar(char a);
+int my_string(char *str);
+int print_int(int num);
+#endif
